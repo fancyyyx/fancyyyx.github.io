56cf6ab6d397cf1dd35d21fdd0a16ee95f37a35b
fancyyyx <131967050+fancyyyx@users.noreply.github.com>
2024-09-07 22:30:14 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,13 +14,13 @@ let myHeading = document.querySelector("h1");
 function setUserName() {
     let myName = prompt("请输入你的名字。");
     localStorage.setItem("name", myName);
-    myHeading.textContent = "Mozilla 酷毙了，" + myName;
+    myHeading.textContent = "欢迎来到我老婆的主页，" + myName;
   }
   if (!localStorage.getItem("name")) {
     setUserName();
   } else {
     let storedName = localStorage.getItem("name");
-    myHeading.textContent = "Mozilla 酷毙了，" + storedName;
+    myHeading.textContent = "欢迎来到我老婆的主页，" + storedName;
   }
   myButton.onclick = function () {
     setUserName();
@@ -33,7 +33,7 @@ function setUserName() {
       setUserName();
     } else {
       localStorage.setItem("name", myName);
-      myHeading.textContent = "Mozilla 酷毙了，" + myName;
+      myHeading.textContent = "欢迎来到我老婆的主页，" + myName;
     }
   }
   
